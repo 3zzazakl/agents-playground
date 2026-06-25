a2231a4382831c63cb00fa62a730ed1199c0e16b
Azzaz Akl <140283548+3zzazakl@users.noreply.github.com>
2026-06-25 19:33:55 +0300

    Refactor next.config.js to simplify configuration
    Removed ESLint and TypeScript build error configurations, and environment variables from nextConfig.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  env: {
-    LIVEKIT_API_KEY: process.env.LIVEKIT_API_KEY,
-    LIVEKIT_API_SECRET: process.env.LIVEKIT_API_SECRET,
-    NEXT_PUBLIC_LIVEKIT_URL: process.env.NEXT_PUBLIC_LIVEKIT_URL,
-  },
+  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
